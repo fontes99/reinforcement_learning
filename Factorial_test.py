abc98d5f2367bf5b3d2fb0d6229703fc3649fa51
fontes99 <pvfontes1000@gmail.com>
2022-03-17 16:32:39 -0300

    go

--- Factorial_test.py
+++
@@ -1,8 +0,0 @@
-import math
-from datetime import datetime
-
-for i in range(1,900000000,1000):
-    inicio = datetime.now()
-    x = math.factorial(i)
-    fim = datetime.now()
-    print(str(i)+" ; "+str(fim-inicio))
--- tests/test_U2.py
+++
@@ -1,9 +0,0 @@
-from utils.SearchAlgorithms import BuscaCustoUniforme
-from scripts.U2 import U2
-
-def test_menor_caminho():
-    state = U2(False, False, False, False, False, ' ')
-    algorithm = BuscaCustoUniforme()
-    result = algorithm.search(state)
-    assert result.g <= 17
-    print(result.show_path())
