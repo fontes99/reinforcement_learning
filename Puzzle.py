750c899ee0c9f3a3a755a9dbe7c6f8c7fab2507e
fontes99 <pvfontes1000@gmail.com>
2022-03-30 10:54:44 -0300

    impossible puzzle checker

--- Puzzle.py
+++ Puzzle.py
@@ -1,8 +1,6 @@
-import numpy as np
 import SearchAlgorithms as sa
 from Graph import State
 from copy import deepcopy
-import math as Math
 
 class Puzzle(State):
 
@@ -24,40 +22,42 @@ class Puzzle(State):
     def sucessors(self):
 
         sucessors = []
+        # print(self.solvable)
 
-        x, y = self.find_N(self.puzzle, 0)
-        
-        # Move UP
-        if x != 0 and self.operator != 'DOWN':
-
-            movUp = deepcopy(self.puzzle)
-            movUp[x][y], movUp[x-1][y] = movUp[x-1][y], movUp[x][y]
+        if self.getParity(self.puzzle) == self.getParity(self.goal):
+            x, y = self.find_N(self.puzzle, 0)
             
-            sucessors.append(Puzzle(movUp, 'UP'))
+            # Move UP
+            if x != 0 and self.operator != 'DOWN':
 
-        # Move DOWN
-        if x != 2 and self.operator != 'UP':
+                movUp = deepcopy(self.puzzle)
+                movUp[x][y], movUp[x-1][y] = movUp[x-1][y], movUp[x][y]
+                
+                sucessors.append(Puzzle(movUp, 'UP'))
 
-            movDown = deepcopy(self.puzzle)
-            movDown[x][y], movDown[x+1][y] = movDown[x+1][y], movDown[x][y]
-            
-            sucessors.append(Puzzle(movDown, 'DOWN'))
+            # Move DOWN
+            if x != 2 and self.operator != 'UP':
 
-        # Move LEFT
-        if y != 0 and self.operator != 'RIGHT':
+                movDown = deepcopy(self.puzzle)
+                movDown[x][y], movDown[x+1][y] = movDown[x+1][y], movDown[x][y]
+                
+                sucessors.append(Puzzle(movDown, 'DOWN'))
 
-            movLeft = deepcopy(self.puzzle)
-            movLeft[x][y], movLeft[x][y-1] = movLeft[x][y-1], movLeft[x][y]
-            
-            sucessors.append(Puzzle(movLeft, 'LEFT'))
+            # Move LEFT
+            if y != 0 and self.operator != 'RIGHT':
 
-        # Move RIGHT
-        if y != 2 and self.operator != 'LEFT':
+                movLeft = deepcopy(self.puzzle)
+                movLeft[x][y], movLeft[x][y-1] = movLeft[x][y-1], movLeft[x][y]
+                
+                sucessors.append(Puzzle(movLeft, 'LEFT'))
 
-            movRight = deepcopy(self.puzzle)
-            movRight[x][y], movRight[x][y+1] = movRight[x][y+1], movRight[x][y]
-            
-            sucessors.append(Puzzle(movRight, 'RIGHT'))
+            # Move RIGHT
+            if y != 2 and self.operator != 'LEFT':
+
+                movRight = deepcopy(self.puzzle)
+                movRight[x][y], movRight[x][y+1] = movRight[x][y+1], movRight[x][y]
+                
+                sucessors.append(Puzzle(movRight, 'RIGHT'))
 
         return sucessors
     
@@ -67,13 +67,14 @@ class Puzzle(State):
         return self.puzzle == self.goal
 
     def description(self):
-        return "Resolve an 8 Puzzle"
+        return "Solve an 8-Puzzle"
     
     def cost(self):
         return 1
 
     def h(self):
 
+        # Sum of manhattan distance for all Ns in puzzle and all Ns in goal
         h = 0
 
         for n in range(9):
@@ -90,34 +91,30 @@ class Puzzle(State):
     def env(self):
         return str([n for line in self.puzzle for n in line])
 
-    # As próximas duas funções foram retiradas de https://www.geeksforgeeks.org/check-instance-8-puzzle-solvable/
-    # A utility function to count
-    # inversions in given array 'arr[]'
-    def getInvCount(self, arr):
+    def getParity(self, puzzle):
         inv_count = 0
-        empty_value = -1
-        for i in range(0, 9):
-            for j in range(i + 1, 9):
-                if arr[j] != empty_value and arr[i] != empty_value and arr[i] > arr[j]:
-                    inv_count += 1
-        return inv_count
+        arr = [j for sub in puzzle for j in sub if j != 0]
 
-    # This function returns true
-    # if given 8 puzzle is solvable.
-    def isSolvable(self, puzzle) :
-    
-        # Count inversions in given 8 puzzle
-        inv_count = self.getInvCount([j for sub in puzzle for j in sub])
-    
-        # return true if inversion count is even.
-        return (inv_count % 2 == 0)
+        for i in range(1, len(arr)):
+            for j in range(i-1, -1, -1):
+                if arr[j] <= arr[j+1]: break
+                arr[j+1], arr[j] = arr[j], arr[j+1];
+                inv_count += 1
+                
+        return inv_count%2
 
 def main():
 
+    # init_state = [
+    #     [8, 1, 3],
+    #     [0, 7, 2],
+    #     [6, 5, 4]
+    # ]
+
     init_state = [
-        [8, 1, 3],
-        [0, 7, 2],
-        [6, 5, 4]
+        [3, 4, 8],
+        [1, 2, 5],
+        [7, 0, 6]
     ]
 
     state = Puzzle(init_state, '')
@@ -129,7 +126,7 @@ def main():
         print('Achou!')
         print(result.show_path())
     else:
-        print('Puzzle impossível')
+        print('Nao achou solucao')
 
 if __name__ == '__main__':
     main()
